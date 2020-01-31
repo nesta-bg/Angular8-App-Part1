58e18620df0d6838ed5b926379e41989b4545f00
nenadnesta <nenadnesta@gmail.com>
2020-01-31 12:26:13 +0100

    Html Attribute vs Dom Property.

--- Angular8-App-Part1/ClientApp/src/app/app.component.ts
+++ Angular8-App-Part1/ClientApp/src/app/app.component.ts
@@ -1,4 +1,3 @@
-//Click on the Component F12 = go to the definition
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,15 +6,11 @@ import { Component } from '@angular/core';
     <div>
       <h1> {{ pageHeader }} </h1>
 
-      <img [src]='imagePath' />
-      <img src='https://www.google.com/images/branding/{{ imagePath1 }}' />
       <button [disabled]='isDisabled'>Click Me</button>
-
-      <span [innerHtml]='pageHeader'></span>
-      <span bind-innerHtml='pageHeader'></span>
-
-      <div>{{ badHtml }}</div>
-      <div [innerHtml]='badHtml'></div>
+      <button disabled='{{ isDisabled }}'>Click Me</button>
+      
+      <br/>
+      <input id='inputId' type='text' value='Tom' >
 
       <my-employee></my-employee>
     </div>
@@ -24,8 +19,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   pageHeader: string = 'Employee Details';
-  imagePath: string = 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png';
-  imagePath1: string = 'googlelogo/1x/googlelogo_color_272x92dp.png';
   isDisabled: boolean = true;
-  badHtml: string = 'Hello <script>alert("Hacked");</script> World';
 }
