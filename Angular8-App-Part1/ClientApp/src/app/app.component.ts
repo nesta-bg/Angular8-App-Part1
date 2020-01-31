30146f455461615b8888baaf02646ca4a2fc5d0d
nenadnesta <nenadnesta@gmail.com>
2020-01-31 10:35:49 +0100

    Angular Interpolation.

--- Angular8-App-Part1/ClientApp/src/app/app.component.ts
+++ Angular8-App-Part1/ClientApp/src/app/app.component.ts
@@ -5,11 +5,24 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <div>
-      <h1> {{ pageHeader }} </h1>
+      <h1> {{ 'Page Header = ' + pageHeader }} </h1>
+      <h1> {{ 10 + 20 + 30 }} </h1>
+      <h1> {{ pageHeader ? pageHeader : 'No Header' }} </h1>
+      <img src='{{ imagePath }}' />
+      <h1> {{ getFullName() }} </h1>
       <my-employee></my-employee>
     </div>
   `
 })
 export class AppComponent {
-  pageHeader: string = 'Employee Details';
+  // pageHeader: string = 'Employee Details';
+  pageHeader: string = null;
+  imagePath: string = 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png';
+
+  firstName: string = 'Tom';
+  lastName: string = 'Hopkins';
+
+  getFullName(): string {
+    return this.firstName + ' ' + this.lastName;
+  }
 }
