4d29c06e9b601a7d59e100470aa1309917a2b26a
nenadnesta <nenadnesta@gmail.com>
2020-01-31 13:52:53 +0100

    Angular Class Binding.

--- Angular8-App-Part1/ClientApp/src/app/app.component.ts
+++ Angular8-App-Part1/ClientApp/src/app/app.component.ts
@@ -4,10 +4,31 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <div>
+      <button class="colorClass" [class]='classesToApply'>My Button</button>
+      <br /><br />
+      <button class="colorClass" [class.boldClass]='!applyBoldClass'>My Button</button>
+      <br /><br />
+      <button class="colorClass italicsClass boldClass" [class.boldClass]='applyBoldClass'>My Button</button>
+      <br /><br />
+      <button class="colorClass" [ngClass]='addClasses()'>My Button</button>
+      <br /><br />
       <my-employee></my-employee>
     </div>
   `
 })
 export class AppComponent {
+  classesToApply: string = 'italicsClass boldClass';
+  applyBoldClass: boolean = false;
+  applyItalicsClass: boolean = true;
+  colorClass: boolean = false;
+
+  addClasses() {
+    let classes = {
+      boldClass: this.applyBoldClass,
+      italicsClass: this.applyItalicsClass,
+      colorClass: this.colorClass
+    };
+    return classes;
+  }
 
 }
