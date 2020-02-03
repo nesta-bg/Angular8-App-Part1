50f818d86790b632a9a98dadcf4c125450131837
nenadnesta <nenadnesta@gmail.com>
2020-02-03 14:16:30 +0100

    Angular Pipes.

--- Angular8-App-Part1/ClientApp/src/app/employee/employeeList.component.ts
+++ Angular8-App-Part1/ClientApp/src/app/employee/employeeList.component.ts
@@ -13,7 +13,7 @@ export class EmployeeListComponent {
         this.employees = [
             {
                 code: 'emp101', name: 'Tom', gender: 'Male',
-                annualSalary: 5500, dateOfBirth: '25/6/1988'
+                annualSalary: 5500, dateOfBirth: '6/25/1988'
             },
             {
                 code: 'emp102', name: 'Alex', gender: 'Male',
@@ -25,7 +25,7 @@ export class EmployeeListComponent {
             },
             {
                 code: 'emp104', name: 'Mary', gender: 'Female',
-                annualSalary: 6500.826, dateOfBirth: '14/10/1980'
+                annualSalary: 6500.826, dateOfBirth: '10/14/1988'
             }
         ]; 
     }
@@ -34,7 +34,7 @@ export class EmployeeListComponent {
         this.employees = [
             {
                 code: 'emp101', name: 'Tom', gender: 'Male',
-                annualSalary: 5500, dateOfBirth: '25/6/1988'
+                annualSalary: 5500, dateOfBirth: '6/25/1988'
             },
             {
                 code: 'emp102', name: 'Alex', gender: 'Male',
@@ -46,11 +46,11 @@ export class EmployeeListComponent {
             },
             {
                 code: 'emp104', name: 'Mary', gender: 'Female',
-                annualSalary: 6500.826, dateOfBirth: '14/10/1980'
+                annualSalary: 6500.826, dateOfBirth: '10/14/1988'
             },
             {
                 code: 'emp105', name: 'Nancy', gender: 'Female',
-                annualSalary: 6700.826, dateOfBirth: '15/12/1982'
+                annualSalary: 6700.826, dateOfBirth: '12/15/1982'
             },
         ];
     }
