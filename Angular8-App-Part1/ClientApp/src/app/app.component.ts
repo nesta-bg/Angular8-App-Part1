87f0f8439bd731f4b2acfbc4f2f827a81f035402
nenadnesta <nenadnesta@gmail.com>
2020-02-03 10:30:12 +0100

    Angular Event Binding.

--- Angular8-App-Part1/ClientApp/src/app/app.component.ts
+++ Angular8-App-Part1/ClientApp/src/app/app.component.ts
@@ -3,33 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <button style='color:red' 
-      [style.font-weight]="isBold ? 'bold' : 'normal'">
-    My Button</button><br/><br/>
-    <button style='color:red' 
-      [style.fontWeight]="isBold ? 'bold' : 'normal'">
-    My Button</button><br/><br/>
+    <button (click)='onClick()'>Click me</button><br/><br/>
+    <button on-click='onClick()'>Click me</button><br/><br/>
 
-    <button style='color:red'
-      [style.font-size.px]="fontSize">
-    My Button</button><br/><br/>
-
-    <button style='color:red' 
-      [ngStyle]="addStyles()">
-    My Button</button>
+    <my-employee></my-employee>
   `
 })
 export class AppComponent {
-  isBold: boolean = true;
-  fontSize: number = 30;
-  isItalic: boolean = true;
-
-  addStyles() {
-    let styles = {
-        'font-weight': this.isBold ? 'bold' : 'normal',
-        'font-style': this.isItalic ? 'italic' : 'normal',
-        'font-size.px': this.fontSize
-    };
-    return styles;
+  onClick(): void {
+    console.log('Button Clicked');
   }
 }
