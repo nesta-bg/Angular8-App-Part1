805f16d0fae51732f2f91d1bcd522824854de2a7
nenadnesta <nenadnesta@gmail.com>
2020-02-04 11:17:44 +0100

    Pass Data from Child Comp. to Parent Comp. - Output Properties.

--- Angular8-App-Part1/ClientApp/src/app/employee/employee-count.component.ts
+++ Angular8-App-Part1/ClientApp/src/app/employee/employee-count.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'employee-count',
@@ -9,4 +9,23 @@ export class EmployeeCountComponent {
   @Input() all: number;
   @Input() male: number;
   @Input() female: number;
+
+  // Holds the selected value of the radio button
+  selectedRadioButtonValue: string = 'All';
+
+  // The Output decorator makes the property an Output property
+  // EventEmitter class is used to create the custom event
+  // When the radio button selection changes, the selected
+  // radio button value which is a string gets passed to the
+  // event handler method. Hence, the event payload is string.
+  @Output() countRadioButtonSelectionChanged: 
+    EventEmitter<string> = new EventEmitter<string>();
+
+  // This method raises the custom event. We will bind this
+  // method to the change event of all the 3 radio buttons
+  onRadioButtonSelectionChange() {
+    this.countRadioButtonSelectionChanged
+      .emit(this.selectedRadioButtonValue);
+  }
+
 }
--- Angular8-App-Part1/ClientApp/src/app/employee/employeeList.component.ts
+++ Angular8-App-Part1/ClientApp/src/app/employee/employeeList.component.ts
@@ -9,6 +9,11 @@ import { Component } from '@angular/core';
 export class EmployeeListComponent {
     employees: any[];
 
+    // This property keeps track of which radio button is selected
+    // We have set the default value to All, so all the employees
+    // are displayed in the table by default
+    selectedEmployeeCountRadioButton: string = 'All';
+
     constructor() {
         this.employees = [
             {
@@ -48,5 +53,16 @@ export class EmployeeListComponent {
 
     getFemaleEmployeesCount(): number {
         return this.employees.filter(e => e.gender === 'Female').length;
+    }
+
+    // Depending on which radio button is selected, this method updates
+    // selectedEmployeeCountRadioButton property declared above
+
+ 
+    // This method is called when the child component (EmployeeCountComponent)
+    // raises the custom event - countRadioButtonSelectionChanged
+    // The event binding is specified in employeeList.component.html
+    onEmployeeCountRadioButtonChange(selectedRadioButtonValue: string): void {
+        this.selectedEmployeeCountRadioButton = selectedRadioButtonValue;
     }
 }
