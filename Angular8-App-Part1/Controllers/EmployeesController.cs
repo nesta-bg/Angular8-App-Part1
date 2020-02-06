f9cdf40abae7f03ccd09ed0911cf1d7c1bc738e3
nenadnesta <nenadnesta@gmail.com>
2020-02-06 13:59:54 +0100

    Creating ASP NET Web API.

---
+++ Angular8-App-Part1/Controllers/EmployeesController.cs
@@ -0,0 +1,34 @@
+﻿using Angular8_App_Part1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angular8_App_Part1.Controllers
+{
+    [Route("/api/employees")]
+    public class EmployeesController : Controller
+    {
+        private readonly AppDbContext context;
+
+        public EmployeesController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Employee>> GetEmployees()
+        {
+            return await context.Employees.ToListAsync();
+            
+        }
+
+        [HttpGet("{code}")]
+        public async Task<Employee> GetEmployee(string code)
+        {
+            return await context.Employees.FirstOrDefaultAsync(e => e.Code == code);
+        }
+    }
+}
