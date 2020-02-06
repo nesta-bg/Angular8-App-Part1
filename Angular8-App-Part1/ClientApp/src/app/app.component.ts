adca4e96538e4725654bca776e79e238bf36a848
nenadnesta <nenadnesta@gmail.com>
2020-02-06 10:46:30 +0100

    Angular Services.

--- Angular8-App-Part1/ClientApp/src/app/app.component.ts
+++ Angular8-App-Part1/ClientApp/src/app/app.component.ts
@@ -3,14 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    Your Text : <input type='text' [(ngModel)]='userText'/>
-    <br/><br/>
-    <simple [simpleInput]='userText'></simple>
+    <list-employee></list-employee>
   `
 })
 
 export class AppComponent {
-  userText: string = 'Manager';
+ 
 }
 
 
--- Angular8-App-Part1/ClientApp/src/app/employee/employeeList.component.ts
+++ Angular8-App-Part1/ClientApp/src/app/employee/employeeList.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IEmployee } from './employee';
+import { EmployeeService } from './employee.service';
 
 @Component({
     selector: 'list-employee',
     templateUrl: './employeeList.component.html',
-    styleUrls: ['./employeeList.component.css']
+    styleUrls: ['./employeeList.component.css'],
+    providers: [EmployeeService]
 })
 
-export class EmployeeListComponent {
+export class EmployeeListComponent implements OnInit {
     employees: IEmployee[];
 
     // This property keeps track of which radio button is selected
@@ -15,33 +17,12 @@ export class EmployeeListComponent {
     // are displayed in the table by default
     selectedEmployeeCountRadioButton: string = 'All';
 
-    constructor() {
-        this.employees = [
-            {
-                code: 'emp101', name: 'Tom', gender: 'Male',
-                annualSalary: 5500, dateOfBirth: '6/25/1988'
-            },
-            {
-                code: 'emp102', name: 'Alex', gender: 'Male',
-                annualSalary: 5700.95, dateOfBirth: '9/6/1982'
-            },
-            {
-                code: 'emp103', name: 'Mike', gender: 'Male',
-                annualSalary: 5900, dateOfBirth: '12/8/1979'
-            },
-            {
-                code: 'emp104', name: 'Mary', gender: 'Female',
-                annualSalary: 6500.826, dateOfBirth: '10/14/1988'
-            },
-            {
-                code: 'emp105', name: 'Nancy', gender: 'Female',
-                annualSalary: 6700.826, dateOfBirth: '12/15/1982'
-            },
-            {
-                code: 'emp106', name: 'Steve', gender: 'Male',
-                annualSalary: 7700.481, dateOfBirth: '11/18/1979'
-            }
-        ]; 
+    constructor(private _employeeService: EmployeeService) {
+    }
+
+    // for tasks that are time consuming
+    ngOnInit() {
+        this.employees = this._employeeService.getEmployees();
     }
 
     getTotalEmployeesCount(): number {
